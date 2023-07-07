b6f77ed372492cf4e53c981ffde0a982f7dac691
Onsongo254 <35399697+Onsongo254@users.noreply.github.com>
2023-07-07 07:28:11 -0700

    Initial commit

---
+++ mailing/mailingvariables.php
@@ -0,0 +1,7 @@
+<?php
+    $mail_host = "smtp.gmail.com";
+    $mail_port = "587";
+    $mail_sender_email = "onskeving@gmail.com"; //sender
+    $mail_sender_password = "Ons7668vek"; //sender
+    $mail_sender_name = "Website Form";
+?>
